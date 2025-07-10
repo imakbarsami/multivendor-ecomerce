e02fee35e8fcae0f0c28d1b21939a9f82fb7e069
Akbar Sami <mdsami6251@gmail.com>
2025-07-10 14:53:27 +0600

    departments table created, add input table in department in admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -14,6 +14,7 @@ use Filament\Support\Colors\Color;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
@@ -58,8 +59,13 @@ class AdminPanelProvider extends PanelProvider
                         RolesEnum::Vendor->value,
                 )
             ])
-            ->authMiddleware([
-                Authenticate::class,
-            ]);
+            // ->authMiddleware([
+            //     Authenticate::class,
+            // ]);
+            ;
+    }
+
+    public function boot(){
+        Model::unguard();
     }
 }
