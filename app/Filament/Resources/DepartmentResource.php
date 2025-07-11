62722bf74934ec562d404e368d79dd8103680ada
Akbar Sami <mdsami6251@gmail.com>
2025-07-11 20:59:24 +0600

    add categories in department through the filament relation manager

--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -69,7 +69,7 @@ class DepartmentResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\CategoriesRelationManager::class,
         ];
     }
 
