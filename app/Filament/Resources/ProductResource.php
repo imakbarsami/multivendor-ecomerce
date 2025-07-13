a800aab8fdf6b5585d0d0fce42bb651c5009be73
Akbar Sami <mdsami6251@gmail.com>
2025-07-13 21:26:36 +0600

    product create, edit implemented

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -2,16 +2,29 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\ProductStatusEnum;
+use App\Enums\RolesEnum;
 use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Category;
+use App\Models\Department;
 use App\Models\Product;
+use Filament\Facades\Filament;
 use Filament\Forms;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+use function Laravel\Prompts\search;
 
 class ProductResource extends Resource
 {
@@ -21,9 +34,90 @@ class ProductResource extends Resource
 
     public static function form(Form $form): Form
     {
+
         return $form
             ->schema([
-                //
+                Grid::make()
+                    ->schema([
+                        TextInput::make('title')
+                            ->required()
+                            ->live(onBlur: true)
+                            ->afterStateUpdated(function (string $operation, $state, callable $set) {
+                                $set('slug', Str::slug($state));
+                            }),
+
+                        TextInput::make('slug'),
+
+                        Select::make('department_id')
+                            ->label('Department')
+                            ->relationship('department', 'name')
+                            ->preload()
+                            ->searchable()
+                            ->reactive()
+                            ->afterStateUpdated(function (callable $set) {
+                                $set('category_id', null);
+                            }),
+
+                        Select::make('category_id')
+                            ->label('Category')
+                            ->relationship(
+                                name: 'category',
+                                titleAttribute: 'name',
+                                modifyQueryUsing: function (Builder $query, callable $get) {
+                                    $departmentId = $get('department_id');
+                                    if ($departmentId) {
+                                        $query->where('department_id', $departmentId);
+                                    }
+                                }
+                            )
+                            ->preload()
+                            ->searchable()
+                            ->reactive()
+                    ]),
+
+                RichEditor::make('description')
+                    ->required()
+                    ->toolbarButtons([
+                        'bold',
+                        'italic',
+                        'link',
+                        'bulletList',
+                        'orderedList',
+                        'h2',
+                        'h3',
+                        'h4',
+                        'blockquote',
+                        'codeBlock',
+                        'table',
+                        'code',
+                        'undo',
+                        'redo',
+                        'underline',
+                        'strike'
+                    ])
+                    ->columnSpan(2),
+
+
+
+                TextInput::make('price')
+                    ->numeric()
+                    ->required(),
+
+                TextInput::make('quantity')
+                    ->integer(),
+
+                Select::make('status')
+                    ->options([
+                        ProductStatusEnum::label()
+                    ])
+                    ->default(ProductStatusEnum::Draft->value)
+                    ->required(),
+
+
+
+
+
+
             ]);
     }
 
@@ -38,6 +132,7 @@ class ProductResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -60,5 +155,11 @@ class ProductResource extends Resource
             'create' => Pages\CreateProduct::route('/create'),
             'edit' => Pages\EditProduct::route('/{record}/edit'),
         ];
+    }
+
+    public static function canViewAny(): bool{
+
+        $user=Filament::auth()->user();
+        return $user && $user->hasRole(RolesEnum::Vendor);
     }
 }
